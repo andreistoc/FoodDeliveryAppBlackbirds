887ec10b12e1513422c666d5ddd67e9b0d487b5f
Andrei Istoc <andreistoc@gmail.com>
2020-12-18 15:18:51 +0200

    Initial Commit

---
+++ FoodDeliveryAppBlackbirds/FoodDeliveryAppBlackbirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppBlackbirdsApp.swift
+//  FoodDeliveryAppBlackbirds
+//
+//  Created by Andrei Istoc on 18.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppBlackbirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
