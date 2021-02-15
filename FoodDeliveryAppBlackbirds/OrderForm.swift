2f6b427ed4223814eed4537d06d73c9949726d60
a sdf <sadfasdf>
2021-02-15 16:43:32 +0200

    Sections Sliders and SFSymbols

--- FoodDeliveryAppBlackbirds/OrderForm.swift
+++ FoodDeliveryAppBlackbirds/OrderForm.swift
@@ -13,19 +13,45 @@ struct OrderForm: View {
     @State var specialRequestInput = ""
     @State var orderAmount = 1
     
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var street = ""
+    @State var city = ""
+    @State var zip = ""
+    
+    @State var userFeedback = 0.0
+    
     var body: some View {
         Form {
-            Toggle(isOn: $specialRequest, label: {
-                Text("Any special Requests?")
-            }).toggleStyle(SwitchToggleStyle(tint: .orange))
-            
-            if specialRequest {
-                TextField("Enter your request", text: $specialRequestInput)
-
+            Section {
+                Toggle(isOn: $specialRequest, label: {
+                    Text("Any special Requests?")
+                }).toggleStyle(SwitchToggleStyle(tint: .orange))
+                
+                if specialRequest {
+                    TextField("Enter your request", text: $specialRequestInput)
+                    
+                }
+                
+                Stepper(value: $orderAmount) {
+                    Text("Quantity: \(orderAmount)")
+                }
             }
             
-            Stepper(value: $orderAmount) {
-                Text("Quantity: \(orderAmount)")
+            Section {
+                TextField("First name", text: $firstName)
+                TextField("Last name", text: $lastName)
+                TextField("Street", text: $street)
+                TextField("City", text: $city)
+                TextField("ZIP code", text: $zip)
+            }
+            
+            Section {
+                HStack {
+                    Image(systemName: "hand.thumbsdown")
+                    Slider(value: $userFeedback, in: 0.0...10.0)
+                    Image(systemName: "hand.thumbsup")
+                }
             }
         }
     }
